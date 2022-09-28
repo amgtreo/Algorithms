2e3b403f67578cc7e19c7921bf7e3724bd6bc366
abmunoz <amg3000@hotmail.com>
2022-09-28 14:24:41 -0500

    - Added refactor for BP - Anagram code - Palindrom code TODO

---
+++ src/main/java/arrays/Palindrome.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.Collections;
+
+public class Palindrome {
+
+    public static void main(String[] args){
+
+    }
+
+    private boolean isPalindrome(String input){
+        char[] str = input.toCharArray();
+        Collections.sort(str);
+    }
+}
